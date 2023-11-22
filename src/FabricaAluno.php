3c4a0ef639c076bf936ebc9938ffba68699af72a
Mikaele Mendonça <mikaelemendonca@gmail.com>
2023-11-21 22:46:36 -0300

    Refatorando a Fabrica de Aluno (Builder)

--- src/FabricaAluno.php
+++ src/FabricaAluno.php
@@ -6,28 +6,43 @@ class FabricaAluno
 {
     private Aluno $aluno;
 
+    private string $numeroCpf;
+    private string $email;
+    private string $nome;
+    private array $telefones = [];
+
+
     public function comCpfEmailENome(
         string $numeroCpf,
         string $email,
         string $nome
     ): self {
-        $this->aluno = new Aluno(
-            new Cpf($numeroCpf),
-            $nome,
-            new Email($email)
-        );
+        $this->numeroCpf = $numeroCpf;
+        $this->email = $email;
+        $this->nome = $nome;
 
         return $this;
     }
 
     public function addTelefone(string $ddd, string $numero): self
     {
-        $this->aluno->addTelefone($ddd, $numero);
+        $this->telefones[] = ['ddd' => $ddd, 'numero' => $numero];
+        
         return $this;
     }
 
     public function getAluno() : Aluno
     {
+        $this->aluno = new Aluno(
+            new Cpf($this->numeroCpf),
+            $this->nome,
+            new Email($this->email)
+        );
+
+        foreach ($this->telefones as $telefone) {
+            $this->aluno->addTelefone($telefone['ddd'], $telefone['numero']);
+        }
+
         return $this->aluno;
     }
 }
